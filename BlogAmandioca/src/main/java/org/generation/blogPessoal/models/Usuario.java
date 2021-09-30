d556ffcd715394552284f5df97054a650af17f79
Amanda <amandaamorim.ctt@gmail.com>
2021-09-29 21:07:00 -0300

    2021.09.29 [Task: CREATE Model class Usuario]

---
+++ BlogAmandioca/src/main/java/org/generation/blogPessoal/models/Usuario.java
@@ -0,0 +1,57 @@
+package org.generation.blogPessoal.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * Cria tabela usuario em db_blog_pessoal e fornece os atributos.
+ * 
+ * @author Amanda
+ * @version 1.0
+ */
+@Entity
+public class Usuario {
+
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long idUsuario;
+	private @NotBlank String nome;
+	private @NotBlank @Email String email;
+	private @NotBlank @Size(min = 5, max = 100) String senha;
+
+	public long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
