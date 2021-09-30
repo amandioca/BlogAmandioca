1369d8265f90a6a9d2760e001bb42b61b0e65a95
Amanda <amandaamorim.ctt@gmail.com>
2021-09-29 21:10:29 -0300

    2021.09.29 [Task: MODIFY TemaModel]

--- BlogAmandioca/src/main/java/org/generation/blogPessoal/models/Tema.java
+++ BlogAmandioca/src/main/java/org/generation/blogPessoal/models/Tema.java
@@ -17,14 +17,8 @@ import javax.validation.constraints.Size;
 public class Tema {
 	
 	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long idTema;
-	
-	@NotBlank
-	@Size(min = 1, max = 20)
-	public String tema;
-	
-	@NotBlank
-	@Size(min = 15, max = 100)
-	public String descricao;
+	private @NotBlank @Size(min = 1, max = 20) String tema;
+	private @NotBlank @Size(min = 15, max = 100) String descricao;
 
 	public long getIdTema() {
 		return idTema;
