a2b38eaeeeb02b98f0d836796596cc5ebfcb3e79
Amanda <amandaamorim.ctt@gmail.com>
2021-10-13 14:00:49 -0300

    [Task: CREATE model.dtos] 2021.10.13

---
+++ BlogAmandioca/src/main/java/org/generation/blogPessoal/models/dtos/UsuarioLoginDTO.java
@@ -0,0 +1,28 @@
+package org.generation.blogPessoal.models.dtos;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UsuarioLoginDTO {
+
+	private @NotBlank @Email(message = "campo deve ser um email") String email;
+	private @NotBlank @Size(min = 5, max = 15, message = "a senha deve ter de 5 à 15 caracteres") String senha;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
