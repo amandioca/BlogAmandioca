799d7b49336bc5f275640efcb3cea2e749b46a70
Amanda <amandaamorim.ctt@gmail.com>
2021-10-13 14:00:01 -0300

    [Task Teste] 2021.10.13

---
+++ BlogAmandioca/src/test/java/org/generation/blogPessoal/repositories/UsuarioRepositoryTest.java
@@ -0,0 +1,14 @@
+package org.generation.blogPessoal.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
